a35791a7b571de35616a0687bfb6764ef3a5700e
Saqib <prosaqib2022@gmail.com>
2024-06-27 00:53:28 +0500

    node and express day 1

--- myApp.js
+++ myApp.js
@@ -1,39 +1,15 @@
 let express = require('express');
 let app = express();
 
+// Express routes method syntax: app.METHOD(PATH, HANDLER)
+
+// app.get("/", (req, res) => {
+//     res.send("Hello Express");
+//   });
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/views/index.html");
+  });
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+  module.exports = app;
