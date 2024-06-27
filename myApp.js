41914365b213851281f8fe8f250bde00dc37fbbb
Saqib <prosaqib2022@gmail.com>
2024-06-27 16:42:01 +0500

    "Building API is easier", They said

--- myApp.js
+++ myApp.js
@@ -1,15 +1,72 @@
+require('dotenv').config()
 let express = require('express');
 let app = express();
-
-// Express routes method syntax: app.METHOD(PATH, HANDLER)
-
-// app.get("/", (req, res) => {
+// app.get("/", (req, res) => { 
 //     res.send("Hello Express");
 //   });
 
-app.get("/", function(req, res) {
-    res.sendFile(__dirname + "/views/index.html");
-  });
+app.use("/public", express.static(__dirname + "/public"));
+
+ app.get("/", function(req, res) {
+     res.sendFile(__dirname + "/views/index.html");
+   });
 
 
-  module.exports = app;
+  //  app.get("/json", (req, res) => { 
+  //   res.json({"message": "Hello json"});
+  //   });
+
+  // app.use(function middleware(req, res, next) {
+  //   console.log(req.method + " " + req.path + " - " + req.ip)  
+  //   next();
+  // });
+  app.get(
+    "/now",
+    (req, res, next) => {
+      req.time = new Date().toString();
+      next();
+    },
+    (req, res) => {
+      res.json({
+        "time": req.time
+      });
+    }
+  );
+  
+  
+  app.get("/json", (req, res) => { if (process.env.MESSAGE_STYLE === "uppercase") {
+    res.json({"message": "HELLO JSON"});
+  } else {
+    res.json({"message": "Hello json"});
+  }
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ module.exports = app;
